f90ef3f9eff6d7429543ec30197c18fa800a3b01
Jordan <jordan.dillard13@gmail.com>
2019-07-16 19:42:22 -0500

    Correct photos rendering with product name and price

---
+++ client/Carousel.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import Slider from "react-slick";
+
+
+
+const Carousel = (props) => {
+    const settings = {
+        dots: false,
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />
+      };
+      //let items = props.items.data;
+
+    return (
+        <div margin="10">
+          <Slider {...settings}>
+      
+            {props.items.map((item) => {
+              return (
+              <div onClick={props.handleClick}> 
+                <img src={`https://hackmazon-images.s3.amazonaws.com/Images/${item.asin}_1.jpg`} height="200" width="160"></img>
+                {item.productTitle} {item.price} 
+              </div>
+              )
+          })}
+          </Slider>  
+        </div>
+    );
+}
+
+
+
+
+
+
+
+
+function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "red" }}
+        onClick={onClick}
+      />
+    );
+  }
+  
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "green" }}
+        onClick={onClick}
+      />
+    );
+  }
+
+export default Carousel;
--- client/index.js
+++ client/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import Slider from "react-slick";
+//import Slider from "react-slick";
 import axios from 'Axios';
+import Carousel from './Carousel.js'
 
-export default class Carousel extends Component {
+export default class App extends Component {
   constructor(props) {
     super(props);
 
@@ -11,7 +12,7 @@ export default class Carousel extends Component {
       currentlyDisplayed : {},
       items: [],
       currentHover: '',
-      clicked: false //when changed to true, send the asin of the currentHover
+      itemClicked: false //when changed to true, send the asin of the currentHover
     };
 
   }
@@ -20,7 +21,7 @@ export default class Carousel extends Component {
       axios.get('/api/items')
         .then((response) => {
           this.setState({
-            items: response
+            items: response.data
           })
           console.log(response);
         }).catch((err) => {
@@ -39,66 +40,40 @@ export default class Carousel extends Component {
   // bc.postMessage('Im an ASIN!');
 
   render() {
-    const settings = {
-      dots: false,
-      infinite: true,
-      slidesToShow: 5,
-      slidesToScroll: 1,
-      nextArrow: <SampleNextArrow />,
-      prevArrow: <SamplePrevArrow />
-    };
+
+    //let items = this.state.items.data;
     return (
-      <div>
-        <h2>Custom Arrows</h2>
-        <Slider {...settings}>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
-        </Slider>
-      </div>
+      <Carousel items={this.state.items} />
+
     );
   }
 }
 
 
 
+ReactDOM.render(<App />, document.getElementById('app'));
 
 
-function SampleNextArrow(props) {
-  const { className, style, onClick } = props;
-  return (
-    <div
-      className={className}
-      style={{ ...style, display: "block", background: "red" }}
-      onClick={onClick}
-    />
-  );
-}
 
-function SamplePrevArrow(props) {
-  const { className, style, onClick } = props;
-  return (
-    <div
-      className={className}
-      style={{ ...style, display: "block", background: "green" }}
-      onClick={onClick}
-    />
-  );
-}
 
-ReactDOM.render(<Carousel />, document.getElementById('app'));
+
+
+
+// <div>
+// <h3>1</h3>
+// </div>
+// <div>
+// <h3>2</h3>
+// </div>
+// <div>
+// <h3>3</h3>
+// </div>
+// <div>
+// <h3>4</h3>
+// </div>
+// <div>
+// <h3>5</h3>
+// </div>
+// <div>
+// <h3>6</h3>
+// </div>
