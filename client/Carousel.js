233dfeb279fdf4487a6d4026660b849cc53d0f9b
Jordan <jordan.dillard13@gmail.com>
2019-07-19 14:53:30 -0500

    deployment testing

--- client/Carousel.js
+++ client/Carousel.js
@@ -7,6 +7,7 @@ import styled from 'styled-components';
 //spacing the carousel
 const CarouselStyled = styled.div`
   margin: 20px;
+  max-widfth: initial;
 `
 
 //css for arrows
@@ -97,7 +98,7 @@ const Carousel = (props) => {
             {props.items.map((item) => {
               return (
               <Items onClick={props.handleClick}> 
-                <img src={`https://hackmazon-thumbs.s3.amazonaws.com/Images/${item.asin}_1.jpg`}
+                <img src={`https://hackmazon-thumbs.s3.amazonaws.com/Images/${item.asin}_1.jpg`} height="180" width="120"
                 onLoad={() => window.dispatchEvent(new Event('resize'))}
                 onClick={() => props.sendAsinBroadcast(item.asin)}></img>
                 <Description>{item.productTitle}</Description>
