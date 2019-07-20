09392b710b1e15c127f8e2e2c71e0a524631c492
Jordan <jordan.dillard13@gmail.com>
2019-07-20 14:15:53 -0500

    fix arrow spacing

--- client/index.js
+++ client/index.js
@@ -4,23 +4,7 @@ import axios from 'Axios';
 import '@brainhubeu/react-carousel/lib/style.css'
 import Carousel from '@brainhubeu/react-carousel';
 import Icon from 'react-fa';
-import styled from 'styled-components';
 import './carouselStyle.css';
-
-
-
-//Styled components
-// const Product = styled.div`
-//  transition: transform .5s ease;
-//  max-width: 20vw;
-
-//   &:hover {
-//     transform: scale(1.04)
-//   }
-// `
-
-
-
 
 
 
@@ -31,10 +15,7 @@ class App extends Component {
     super(props);
 
     this.state = {
-      currentlyDisplayed : {},
       items: [],
-      images:[],
-      itemClicked: false //when changed to true, send the asin of the currentHover
     };
 
     this.sendAsinBroadcast = this.sendAsinBroadcast.bind(this);
@@ -47,6 +28,7 @@ class App extends Component {
   }
 
 
+  //gets random sample of 14 products
   getRandomItems() {
     axios.get('http://carouseltest-dev.us-east-1.elasticbeanstalk.com/api/items')
     .then((response) => {
@@ -58,28 +40,16 @@ class App extends Component {
     })
   }
 
-  // const bc = new BroadcastChannel('product-change');
-  //   bc.onmessage = function (ev) { 
-  //     console.log('Changing the product to ' + ev.data + ', boss!'); 
-  //   } //replace with your handler
 
-
+//broadcasts ASIN to render new product
   sendAsinBroadcast(asin) {
     const bc = new BroadcastChannel('product-change');
     bc.postMessage(asin);
   }
   
 
-  getAllImages() {
-
-  }
-
 
   render() {
-    // let asin = 
-    // let slides = function() {
-
-    // }
 
     return (
       <>
@@ -94,7 +64,7 @@ class App extends Component {
         slidesPerScroll={2}
         slides={[]}
         breakpoints={{
-          1024: { // these props will be applied when screen width is less than 1000px
+          1024: { // these props will be applied when screen width is less than 1024px
             slidesPerPage: 3,
             clickToChange: false,
             centered: false,
@@ -124,7 +94,6 @@ class App extends Component {
               return (
               <div className="eachProduct" key={item.productTitle}> 
                 <img src={`https://hackmazon-thumbs.s3.amazonaws.com/Images/${item.asin}_1.jpg`} height="160" width="120"
-                onLoad={() => window.dispatchEvent(new Event('resize'))}
                 onClick={() => {
                   this.sendAsinBroadcast(item.asin)
                   let that = this
