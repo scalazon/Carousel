9e2a5f331772b156c122dcc4b36e5655a649cf34
Jordan <jordan.dillard13@gmail.com>
2019-07-20 11:32:04 -0500

    Line clamp with ellipses working and productTitle displyed correctly

--- client/index.js
+++ client/index.js
@@ -5,6 +5,7 @@ import '@brainhubeu/react-carousel/lib/style.css'
 import Carousel from '@brainhubeu/react-carousel';
 import Icon from 'react-fa';
 import styled from 'styled-components';
+import './carouselStyle.css';
 
 
 
@@ -107,12 +108,12 @@ class App extends Component {
         > 
             {this.state.items.map((item) => {
               return (
-              <div key={item.productTitle}> 
+              <div className="eachProduct" key={item.productTitle}> 
                 <img src={`https://hackmazon-thumbs.s3.amazonaws.com/Images/${item.asin}_1.jpg`} height="160" width="120"
                 onLoad={() => window.dispatchEvent(new Event('resize'))}
                 onClick={() => this.sendAsinBroadcast(item.asin)}></img>
-                <div>{item.productTitle}</div>
-                <div color="#B12704">${item.price}<img src ="http://www.sclance.com/pngs/amazon-prime-logo-png/amazon_prime_logo_png_31200.png" height="25" width="48"></img></div>
+                <div className="nameOfProduct">{item.productTitle}</div>
+                <div className="priceC">${item.price}<img src ="http://www.sclance.com/pngs/amazon-prime-logo-png/amazon_prime_logo_png_31200.png" height="25" width="48"></img></div>
               </div>
               )
           })}
