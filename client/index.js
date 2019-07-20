4978ffadfb48a19a357a4e24ada18592f219e15b
Jordan <jordan.dillard13@gmail.com>
2019-07-19 23:25:14 -0500

    installed react fa for the arrows

--- client/index.js
+++ client/index.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-//import Slider from "react-slick";
 import axios from 'Axios';
-import HackCarousel from './Carousel.js';
 import '@brainhubeu/react-carousel/lib/style.css'
 import Carousel from '@brainhubeu/react-carousel';
+import Icon from 'react-fa';
 
 
 
@@ -62,16 +61,35 @@ class App extends Component {
       <Carousel 
         items={this.state.items} 
         sendAsinBroadcast={this.sendAsinBroadcast}
+        arrowLeft={<Icon className="left" name="arrow-left" />}
+        arrowRight={<Icon className="right" name="arrow-right" />}
         slidesPerPage={6}
         slidesPerScroll={2}
         slides={[1,2,3,4,5,6,7,8,9,0,10]}
+        breakpoints={{
+          1000: { // these props will be applied when screen width is less than 1000px
+            slidesPerPage: 2,
+            clickToChange: false,
+            centered: false,
+            arrows: true,
+            infinite: false,
+          },
+          500: {
+            slidesPerPage: 1,
+            slidesPerScroll: 1,
+            clickToChange: false,
+            centered: false,
+            animationSpeed: 2000,
+            infinite: false,
+          },
+        }}
         arrows
         > 
             {this.state.items.map((item) => {
               return (
               <div key={item.productTitle}> 
                 <img src={`https://hackmazon-thumbs.s3.amazonaws.com/Images/${item.asin}_1.jpg`} height="160" width="120"
-                //onLoad={() => window.dispatchEvent(new Event('resize'))}
+                onLoad={() => window.dispatchEvent(new Event('resize'))}
                 onClick={() => this.sendAsinBroadcast(item.asin)}></img>
                 <div>{item.productTitle}</div>
                 <div>${item.price}<img src ="http://www.sclance.com/pngs/amazon-prime-logo-png/amazon_prime_logo_png_31200.png" height="25" width="48"></img></div>
