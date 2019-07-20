ad2c39efffcaeb845a855312c168142e7bab7e39
Jordan <jordan.dillard13@gmail.com>
2019-07-20 12:25:10 -0500

    Items rerender on click

--- client/index.js
+++ client/index.js
@@ -38,20 +38,25 @@ class App extends Component {
     };
 
     this.sendAsinBroadcast = this.sendAsinBroadcast.bind(this);
+    this.getRandomItems = this.getRandomItems.bind(this);
 
   }
 
   componentDidMount() {
-      axios.get('http://carouseltest-dev.us-east-1.elasticbeanstalk.com/api/items')
-        .then((response) => {
-          this.setState({
-            items: response.data
-          })
-        }).catch((err) => {
-          console.log('Err in componentDidMount:', err);
-        })
+    this.getRandomItems();
   }
 
+
+  getRandomItems() {
+    axios.get('http://carouseltest-dev.us-east-1.elasticbeanstalk.com/api/items')
+    .then((response) => {
+      this.setState({
+        items: response.data
+      })
+    }).catch((err) => {
+      console.log('Err in componentDidMount:', err);
+    })
+  }
 
   // const bc = new BroadcastChannel('product-change');
   //   bc.onmessage = function (ev) { 
@@ -120,7 +125,10 @@ class App extends Component {
               <div className="eachProduct" key={item.productTitle}> 
                 <img src={`https://hackmazon-thumbs.s3.amazonaws.com/Images/${item.asin}_1.jpg`} height="160" width="120"
                 onLoad={() => window.dispatchEvent(new Event('resize'))}
-                onClick={() => this.sendAsinBroadcast(item.asin)}></img>
+                onClick={() => {
+                  this.sendAsinBroadcast(item.asin)
+                  this.getRandomItems()
+                  }}></img>
                 <div className="nameOfProduct">{item.productTitle}</div>
                 <div className="priceC">${item.price}<img src ="http://www.sclance.com/pngs/amazon-prime-logo-png/amazon_prime_logo_png_31200.png" height="25" width="48"></img></div>
               </div>
