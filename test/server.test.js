c9d2f16896ed2f4d98ac574c2b623e7e0359d72f
Evelyn Binkard <evelyn.binkard@gmail.com>
2019-07-26 11:24:03 -0500

    added server test again

--- test/server.test.js
+++ test/server.test.js
@@ -9,13 +9,13 @@ test('adds 1 + 2 to equal 3', () => {
     expect (sum(1, 2)).toBe(3);
 });
 
-// test('gets response from server', () => {
+test('gets response from server', () => {
     
-//     return axios.get('http://localhost:3003/test')
-//         .then(response => {
-//             expect(response.data).toEqual('done');
-//         })
-// })
+    return axios.get('http://localhost:3003/test')
+        .then(response => {
+            expect(response.data).toEqual('done');
+        })
+})
 
 // // test('retrieves test item from database', () => {
 // //     return axios.get('http://localhost:3003/one')
